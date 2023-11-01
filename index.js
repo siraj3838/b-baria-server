cd14e07a984669a1b26e9e8e3cc0af4f17add2da
Sirajul Islam <ayasabegum134@gmail.com>
2023-11-01 23:38:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5039;
+
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}))
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hq29e8f.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+// middlewares
+const logger = (req, res, next) =>{
+  console.log('log: info',req.method, req.url);
+  next();
+}
+
+const verifyToken = (req, res, next)=> {
+  const token = req?.cookies?.token;
+  // console.log('token in the middleware',token);
+  // no tokens
+  if(!token){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    req.user = decoded;
+    next();
+  })
+}
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const donerCollection = client.db('donerDB').collection('doner');
+
+
+    // auth related
+    app.post('/jwt', logger, async (req, res) => {
+      const user = req.body;
+      console.log('user for token', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '12h' });
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      })
+      .send({success: true})
+    })
+
+    app.post('/logout', async(req,res)=>{
+      const user = req.body;
+      console.log('login out', user);
+      res.clearCookie('token', {maxAge: 0}).send({success: true})
+    })
+
+
+// doner related
+    app.post('/doners', logger, async (req, res) => {
+      const user = req.body;
+      const result = await donerCollection.insertOne(user);
+      res.send(result);
+      // console.log(result)
+    })
+    app.get('/doners', logger, async (req, res) => {
+      // console.log( 'cook cook token owner',req.user)
+      const result = await donerCollection.find().toArray();
+      res.send(result);
+      // console.log(result);
+    })
+
+    // app.get('/doners', logger, verifyToken, async (req, res) => {
+      
+    //   console.log('email',req.query.email);
+    //   let query = {};
+    //   if (req?.query?.email) {
+    //     query = { email: req.query.email }
+    //   }
+    //   const result = await donerCollection.find(query).toArray();
+    //   res.send(result)
+
+    // })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('simple crud running Blood donate');
+})
+app.listen(port, () => {
+  console.log(`My donate Was Running from ${port}`)
+})
+
+
+
